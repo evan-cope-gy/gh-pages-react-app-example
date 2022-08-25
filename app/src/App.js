2e1ce79563eb6b04624cdd127db9f4d9e690f264
evan-cope-gy <evan_cope@goodyear.com>
2022-08-25 13:45:59 -0400

    Basic app up and running

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import swirl from './swirl.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={swirl} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
